066f065f16c59d942f358cef1b103c3a1be2b06c
mikaellafs <mikaellaferreira0@gmail.com>
2022-07-04 21:21:51 -0300

    Organize beacon service

--- contact_tracing/app/src/main/java/pg/contact_tracing/domain/usecases/BeaconServiceUseCase.java
+++ contact_tracing/app/src/main/java/pg/contact_tracing/domain/usecases/BeaconServiceUseCase.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 
 import androidx.core.content.ContextCompat;
 
+import pg.contact_tracing.R;
 import pg.contact_tracing.infra.services.BeaconService;
 
 public class BeaconServiceUseCase {
     public void start(Context context) {
         Intent serviceIntent = new Intent(context, BeaconService.class);
-        serviceIntent.putExtra("inputExtra", "@strings/");
+
+        serviceIntent.putExtra(
+                context.getString(R.string.beacon_notification_subtitle_field),
+                context.getString(R.string.beacon_notification_subtitle)
+        );
+
         ContextCompat.startForegroundService(context, serviceIntent);
     }
     public void stop(Context context) {
--- contact_tracing/app/src/main/java/pg/contact_tracing/infra/services/BeaconService.java
+++ contact_tracing/app/src/main/java/pg/contact_tracing/infra/services/BeaconService.java
@@ -6,12 +6,9 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseSettings;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -22,7 +19,6 @@ import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.BeaconTransmitter;
 import org.altbeacon.beacon.Identifier;
-import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
@@ -32,40 +28,33 @@ import java.util.Collection;
 
 import pg.contact_tracing.MainActivity;
 import pg.contact_tracing.R;
+import pg.contact_tracing.domain.usecases.UserContactsUseCase;
+import pg.contact_tracing.domain.usecases.UserInformationsUseCase;
 
 public class BeaconService extends Service {
     public static final String CHANNEL_ID = "ForegroundServiceChannel";
+    public static final String LOG_KEY_TRANSMIT = "BEACON_SERVICE_TRANSMIT";
+    public static final String LOG_KEY_MONITOR = "BEACON_SERVICE_MONITOR";
+
+    private UserInformationsUseCase userInformationsUseCase;
+    private UserContactsUseCase userContactsUseCase;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        userInformationsUseCase = new UserInformationsUseCase();
+        userContactsUseCase = new UserContactsUseCase();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String input = intent.getStringExtra("inputExtra");
-        createNotificationChannel();
-
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                0, notificationIntent, 0);
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Beacon Service")
-                .setContentText(input)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentIntent(pendingIntent)
-                .build();
+        Notification notification = createNotification(intent);
 
         startForeground(1, notification);
-        //do heavy work on a background thread
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-//                transmitBeacon();
-                monitorBeacons();
-            }
-        });
-        //stopSelf();
+
+        transmitBeacon();
+        monitorBeacons();
+
         return START_NOT_STICKY;
     }
 
@@ -82,66 +71,74 @@ public class BeaconService extends Service {
     private void createNotificationChannel() {
         NotificationChannel serviceChannel = new NotificationChannel(
                 CHANNEL_ID,
-                "Foreground Service Channel",
+                getString(R.string.beacon_service_channel),
                 NotificationManager.IMPORTANCE_DEFAULT
         );
         NotificationManager manager = getSystemService(NotificationManager.class);
         manager.createNotificationChannel(serviceChannel);
     }
 
+    private Notification createNotification(Intent intent) {
+        String subtitle = intent.getStringExtra(getString(R.string.beacon_notification_subtitle_field));
+        createNotificationChannel();
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle(getString(R.string.beacon_notification_title))
+                .setContentText(subtitle)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        return notification;
+    }
+
     private void transmitBeacon(){
-        BluetoothManager btManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-        BluetoothAdapter btAdapter = btManager.getAdapter();
-        if (btAdapter.isEnabled()) {
-            boolean isSupported = btAdapter.isMultipleAdvertisementSupported();
-
-        } else {
-            // Turn on bluetooth
-        }
-
         Beacon beacon = new Beacon.Builder()
-                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId1(userInformationsUseCase.getUUID())
                 .setId2("1")
                 .setId3("2")
-//                .setManufacturer(0x0118) // Radius Networks.  Change this for other beacon layouts
+                .setManufacturer(userInformationsUseCase.getAppManufacturer())
                 .setTxPower(-59)
                 .setDataFields(Arrays.asList(new Long[] {0l})) // Remove this for beacon layouts without d: fields
                 .build();
 
-        // Change the layout below for other beacon types
         BeaconParser beaconParser = new BeaconParser()
-                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+                .setBeaconLayout(BeaconParser.ALTBEACON_LAYOUT);
         BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
         beaconTransmitter.startAdvertising(beacon, new AdvertiseCallback() {
-
             @Override
             public void onStartFailure(int errorCode) {
-                Log.e("BATATA", "Advertisement start failed with code: "+errorCode);
+                Log.e(LOG_KEY_TRANSMIT, "Advertisement start failed with code: " + errorCode);
             }
 
             @Override
             public void onStartSuccess(AdvertiseSettings settingsInEffect) {
-                Log.i("BATATA", "Advertisement start succeeded.");
+                Log.i(LOG_KEY_TRANSMIT, "Advertisement start succeeded.");
             }
         });
     }
 
-    void monitorBeacons() {
-        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
-        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-        // beaconManager.getBeaconParsers().add(new BeaconParser().
-        //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-
+    private void monitorBeacons() {
         BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.addRangeNotifier(new RangeNotifier() {
             @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region)
-            {
-                Log.i("BATATA","didRangeBeaconsInRegion, number of beacons detected = "+beacons.size());
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
                 if (beacons.size() > 0) {
-                    Log.i("BATATA", "didRangeBeaconsInRegion, beacon = " + beacons.iterator().next().toString());
+                    userContactsUseCase.saveBeacon(beacons);
+
+                    for (Beacon beacon: beacons) {
+                        Log.i(LOG_KEY_MONITOR, "didRangeBeaconsInRegion, beacon = " + beacon.toString());
+
+                        if (beacon.getDistance() <= 1.0) {
+                            Log.i(LOG_KEY_MONITOR, "Very close beacon: " + beacon.getDistance());
+                        } else {
+                            Log.i(LOG_KEY_MONITOR, "Far beacon, discard: " + beacon.getDistance());
+                        }
+                    }
                 }
-                // HERE IT IS : the size is Always 1, but the beacon (UUID etc. can be different)
             }
         });
 
@@ -150,3 +147,7 @@ public class BeaconService extends Service {
         beaconManager.startRangingBeacons(new Region("aaa", identifiers));
     }
 }
+
+//    AsyncTask.execute(new Runnable() {
+//        @Override public void run() { }
+//    });
