925ba3c599dc7d9161d84b32a2df0e5cea647f2d
mikaellafs <mikaellaferreira0@gmail.com>
2022-09-30 19:32:28 -0300

    Add receiver to main activity to set warning banner

--- contact_tracing/app/src/main/java/pg/contact_tracing/ui/activities/MainActivity.java
+++ contact_tracing/app/src/main/java/pg/contact_tracing/ui/activities/MainActivity.java
@@ -3,7 +3,9 @@ package pg.contact_tracing.ui.activities;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,12 +21,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.util.ArrayList;
 
 import pg.contact_tracing.R;
 import pg.contact_tracing.ui.fragments.WarningBanner;
 import pg.contact_tracing.utils.BeaconServiceManager;
+import pg.contact_tracing.utils.NotificationBroadcastCenter;
 
 public class MainActivity extends AppCompatActivity {
     private static final String MAIN_ACTIVITY_LOG = "MAIN_ACTIVITY";
@@ -33,12 +37,28 @@ public class MainActivity extends AppCompatActivity {
     private TextView tracingSubtitle;
     private ImageView tracingImage;
 
+    private WarningBanner warningBanner;
+
     BeaconServiceManager beaconServiceManager;
 
     private String locationPermission = Manifest.permission.ACCESS_FINE_LOCATION;
     private String bluetoothAdvertisePermission = Manifest.permission.BLUETOOTH_ADVERTISE;
     private String bluetoothScanPermission = Manifest.permission.BLUETOOTH_SCAN;
     private String bluetoothConnectPermission = Manifest.permission.BLUETOOTH_CONNECT;
+
+    BroadcastReceiver userNotAtRiskReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            hideWarningBanner();
+        }
+    };
+
+    BroadcastReceiver userAtRiskReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            showWarningBanner(intent.getStringExtra("message"));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
         setSwitchAction();
         setWarningBanner();
+        setReceiversNotification();
     }
 
     private void setSwitchAction() {
@@ -69,23 +90,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setWarningBanner() {
+    private void setReceiversNotification() {
+        Log.i(MAIN_ACTIVITY_LOG, "Setting receivers about user at risk to show or hide banner");
+
+        // Listen to event user not at risk - hide banner
+        NotificationBroadcastCenter.registerReceiver(this, NotificationBroadcastCenter.Event.NOT_RISK_NOTIFICATION, userNotAtRiskReceiver);
+
+        // Listen to event user at risk - show banner
+        NotificationBroadcastCenter.registerReceiver(this, NotificationBroadcastCenter.Event.RISK_NOTIFICATION, userAtRiskReceiver);
+    }
+
+    private void hideWarningBanner() {
+        Log.i(MAIN_ACTIVITY_LOG, "Hide warning banner fragment");
+
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.banner, new WarningBanner());
+        ft.hide(warningBanner);
+        ft.commit();
+    }
+
+    private void showWarningBanner(String message) {
+        Log.i(MAIN_ACTIVITY_LOG, "Show warning banner fragment");
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.show(warningBanner);
+        ft.commit();
+
+        warningBanner.setMessage(message);
+    }
+
+    private void setWarningBanner() {
+        warningBanner = new WarningBanner();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.banner, warningBanner);
+
         ft.commit();
     }
 
     private void enableBluetooth() {
+        Log.i(MAIN_ACTIVITY_LOG, "Enable bluetooth");
         BluetoothManager btManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         BluetoothAdapter btAdapter = btManager.getAdapter();
 
         if (!btAdapter.isEnabled()) {
             // Show message that bluetooth was turned on
+            // TODO: GET PERMISSIONS BEFORE
             btAdapter.enable();
         }
     }
 
     private boolean checkAndRequestPermissions(Context context) {
+        Log.i(MAIN_ACTIVITY_LOG, "Checking permissions");
         ArrayList<String> permissionsNotGranted = new ArrayList<>();
         String[] permissions;
 
@@ -99,12 +153,16 @@ public class MainActivity extends AppCompatActivity {
         for (String permission : permissions) {
             boolean isGranted = ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
 
-            if (!isGranted)
+            if (!isGranted) {
+                Log.i(MAIN_ACTIVITY_LOG, "Permission: " + permission + " is not granted");
                 permissionsNotGranted.add(permission);
+            }
         }
 
         if (!permissionsNotGranted.isEmpty())
             ActivityCompat.requestPermissions(this, permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), 1);
+        else
+            Log.i(MAIN_ACTIVITY_LOG, "All permissions required are granted");
 
         return permissionsNotGranted.isEmpty();
     }
@@ -112,6 +170,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.i(MAIN_ACTIVITY_LOG, "Request permissions result");
 
         Context context = getApplicationContext();
 
@@ -135,25 +194,36 @@ public class MainActivity extends AppCompatActivity {
         }
         enableBluetooth();
 
+        Log.i(MAIN_ACTIVITY_LOG, "Start tracing...");
         beaconServiceManager.start(context);
         setToTracingMode();
         return true;
     }
 
     public void stopTracing(Context context) {
+        Log.i(MAIN_ACTIVITY_LOG, "Stop tracing...");
         beaconServiceManager.stop(context);
         setToNotTracingMode();
     }
 
     private void setToTracingMode() {
+        Log.i(MAIN_ACTIVITY_LOG, "Setting activity to tracing mode");
         tracingTitle.setText(R.string.main_tracing_active_title);
         tracingSubtitle.setText(R.string.main_tracing_active_subtitle);
         tracingImage.setBackgroundResource(R.drawable.illustration_tracing);
     }
 
     private void setToNotTracingMode() {
+        Log.i(MAIN_ACTIVITY_LOG, "Setting activity to NOT tracing mode");
         tracingTitle.setText(R.string.main_tracing_inactive_title);
         tracingSubtitle.setText(R.string.main_tracing_inactive_subtitle);
         tracingImage.setBackgroundResource(R.drawable.illustration_not_tracing);
+    }
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(userAtRiskReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(userNotAtRiskReceiver);
+        super.onDestroy();
     }
 }
